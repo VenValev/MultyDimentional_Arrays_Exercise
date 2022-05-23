0cac75d09a94b8c65eaff6ce002a34e776d96d9c
Vencislav <vencislav67@hotmail.com>
2022-05-23 14:11:47 +0300

    Made some methods for finding the matrix. See if it works

--- 03_MaxSum/Program.cs
+++ 03_MaxSum/Program.cs
@@ -14,8 +14,56 @@ namespace _03_MaxSum
             int[,] wantedMatrix = new int[3, 3];
 
             FillingTheMatrix(mainMatrix, rows, cols);
+
+            FindingTheMatrix(mainMatrix, rows, cols, wantedMatrix);
         }
 
+        private static void FindingTheMatrix(int[,] mainMatrix, int rows, int cols, int[,] wantedMatrix)
+        {
+            int maxSum = int.MinValue;
+            for(int i=0; i < rows - 3; i++)
+            {
+                for(int j=0; j < cols - 3; j++)
+                {
+                    /*int sum = 
+                        mainMatrix[i, j] + mainMatrix[i,j+1] + mainMatrix[i, j+2] +
+                        mainMatrix[i+1, j] + mainMatrix[i+1, j + 1] + mainMatrix[i+1, j + 2] + 
+                        mainMatrix[i+2, j] + mainMatrix[i+2, j + 1] + mainMatrix[i+2, j + 2];*/
+                    int sum = 0;
+                    sum = SumOfMatrix(mainMatrix, i, j, sum);
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        FillingWantedMatrix(mainMatrix, i, j, wantedMatrix);
+                    }
+                }
+            }
+        }
+
+        private static void FillingWantedMatrix(int[,] mainMatrix, int i, int j, int[,] wantedMatrix)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3, c++)
+                {
+                    wantedMatrix[r,c] = mainMatrix[r + i, c + j];
+                }
+            }
+        }
+
+        static int SumOfMatrix(int[,] mainMatrix,int i, int j, int sum)
+        {
+            for(int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 3, c++)
+                {
+                    sum += mainMatrix[r + i, c + j];
+                }
+            }
+
+            return sum;
+        }
         static void FillingTheMatrix(int[,] matrix, int rows, int cols)
         {
             for (int r = 0; r < rows; r++)
