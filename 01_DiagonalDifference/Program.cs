188595ad9752137df829287ecb255c04b8d60e3b
Vencislav <vencislav67@hotmail.com>
2022-05-23 11:15:32 +0300

    Add project files.

---
+++ 01_DiagonalDifference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
