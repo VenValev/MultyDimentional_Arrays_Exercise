a9c7bd747f22da3c8f061a0a8c9834e7bcac1105
Vencislav <vencislav67@hotmail.com>
2022-05-26 16:40:36 +0300

    Started problem n7.

---
+++ 07_TheKnightGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _07_TheKnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] board = new int[n, n];
+        }
+    }
+}
