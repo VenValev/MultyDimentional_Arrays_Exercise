df20f5a783ea4831b4850b653778d87eb16878f1
Vencislav <vencislav67@hotmail.com>
2022-05-26 17:40:59 +0300

    ifs

--- 07_TheKnightGame/Program.cs
+++ 07_TheKnightGame/Program.cs
@@ -12,28 +12,75 @@ namespace _07_TheKnightGame
 
             FillingBoard(board, n);
 
-            RemovingKnights(board, n, br);
+            br = RemovingKnights(board, n, br);
+
+            Console.WriteLine(br);
         }
 
-        private static void RemovingKnights(string[,] board, int n, int br)
+        private static int RemovingKnights(string[,] board, int n, int br)
         {
             for(int r = 0; r < n; r++)
             {
                 for(int c = 0; c < n; c++)
                 {
-
+                    if (board[r, c] == "K")
+                    {
+                        if(ChekIfThereIsKnight(board, n, br, r, c))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            br++;
+                        }
+                    }
                 }
             }
+            return br;
+        }
+
+        private static bool ChekIfThereIsKnight(string[,] board, int n, int br, int r, int c)
+        {
+            int[] rowMove = { 2, 1, -1, -2, -2, -1,  1,  2 };
+            int[] colMove = { 1, 2,  2,  1, -1, -2, -2, -1 };
+            for(int i = 0; i < n; i++)
+            {
+                if (r + rowMove[i] < 0 || c + colMove[i] < 0)
+                {
+                    continue;
+                }
+                else if(r + rowMove[i] >= n || c + colMove[i] >= n)
+                {
+                    continue;
+                }
+
+            }
+
+            /*if (board[r-2, c-1] != "K" &&
+                board[r-2, c+1] != "K" &&
+                board[r-1, c-2] != "K" &&
+                board[r-1, c+2] != "K" &&
+                board[r+1, c-2] != "K" &&
+                board[r+1, c+2] != "K" &&
+                board[r+2, c-1] != "K" &&
+                board[r+2, c+1] != "K")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }*/
         }
 
         private static void FillingBoard(string[,] board, int n)
         {
             for (int r = 0; r < n; r++)
             {
-                string[] s = Console.ReadLine().Split();
+                string s = Console.ReadLine();
                 for (int c = 0; c < n; c++)
                 {
-                    board[r, c] = s[c];
+                    board[r, c] = s[c].ToString();
                 }
             }
         }
