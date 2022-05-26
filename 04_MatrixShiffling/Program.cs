d6c5f8fa6447666477b49a7c552c56286c153758
Vencislav <vencislav67@hotmail.com>
2022-05-26 12:11:41 +0300

    Filling the matrix.

--- 04_MatrixShiffling/Program.cs
+++ 04_MatrixShiffling/Program.cs
@@ -10,6 +10,21 @@ namespace _04_MatrixShiffling
             int rows = int.Parse(demetions[0]);
             int cols = int.Parse(demetions[1]);
             string[,] matrix = new string[rows, cols];
+
+            FillingTheMatrix(matrix, rows, cols);
+        }
+
+        private static void FillingTheMatrix(string[,] matrix, int rows, int cols)
+        {
+            for(int r = 0; r < rows; r++)
+            {
+                string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for(int c = 0; c < cols; c++)
+                {
+                    matrix[r, c] = line[c];
+                }
+            }
         }
     }
 }
