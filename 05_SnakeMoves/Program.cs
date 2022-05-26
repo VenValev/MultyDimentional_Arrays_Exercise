5e1121c0be30b9af2e9c1520a1f72f72c5abcb9f
Vencislav <vencislav67@hotmail.com>
2022-05-26 14:17:22 +0300

    Created a new project for problem N5. Initiated the variables.

---
+++ 05_SnakeMoves/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _05_SnakeMoves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dimetions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string snake = Console.ReadLine();
+
+
+        }
+    }
+}
