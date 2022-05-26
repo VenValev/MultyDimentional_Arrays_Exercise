6cb7719b1298fc450cf660f3ac58f68d43fad28e
Vencislav <vencislav67@hotmail.com>
2022-05-26 16:49:31 +0300

    Starting to find the knights

--- 07_TheKnightGame/Program.cs
+++ 07_TheKnightGame/Program.cs
@@ -8,16 +8,33 @@ namespace _07_TheKnightGame
         {
             int n = int.Parse(Console.ReadLine());
             string[,] board = new string[n,n];
+            int br = 0;
 
+            FillingBoard(board, n);
+
+            RemovingKnights(board, n, br);
+        }
+
+        private static void RemovingKnights(string[,] board, int n, int br)
+        {
             for(int r = 0; r < n; r++)
             {
-                string[] s = Console.ReadLine().Split();
                 for(int c = 0; c < n; c++)
                 {
-                    board[r,c] = s[c];
+
                 }
+            }
+        }
 
-
+        private static void FillingBoard(string[,] board, int n)
+        {
+            for (int r = 0; r < n; r++)
+            {
+                string[] s = Console.ReadLine().Split();
+                for (int c = 0; c < n; c++)
+                {
+                    board[r, c] = s[c];
+                }
             }
         }
     }
