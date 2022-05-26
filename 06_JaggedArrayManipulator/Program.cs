532dd39b6c69f39a278e5895ac22d7b4bee28fa0
Vencislav <vencislav67@hotmail.com>
2022-05-26 15:05:43 +0300

    Started initializing the commands.

--- 06_JaggedArrayManipulator/Program.cs
+++ 06_JaggedArrayManipulator/Program.cs
@@ -15,21 +15,36 @@ namespace _06_JaggedArrayManipulator
 
             AnalizingJMatrix(jMatrix, rows);
 
+            string cmnd;
+
+            while((cmnd = Console.ReadLine()) !="End")
+            {
+                string[] cmndArg = cmnd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmndArg[0] == "Add")
+                {
+
+                }
+                else if (cmndArg[1] == "Subtract")
+                {
+
+                }
+            }
+
         }
 
         private static void AnalizingJMatrix(int[][] jMatrix, int rows)
         {
-            for(int r = 0; r < rows; r++)
+            for(int r = 0; r < rows-1; r++)
             {
                 if(jMatrix[r].Length == jMatrix[r+1].Length )
                 {
                     for(int c = 0; c < jMatrix[r].Length; c++)
                     {
-                        if(r < rows-1)
-                        {
-                            jMatrix[r][c] = jMatrix[r][c] * 2;
-                            jMatrix[r + 1][c] = jMatrix[r + 1][c] * 2;
-                        }
+                        
+                        jMatrix[r][c] = jMatrix[r][c] * 2;
+                        jMatrix[r + 1][c] = jMatrix[r + 1][c] * 2;
+                        
                         
                     }
                 }
@@ -37,7 +52,8 @@ namespace _06_JaggedArrayManipulator
                 {
                     for(int c = 0; c < jMatrix[r].Length; c++)
                     {
-                        jMatrix[r][c] = jMatrix[r][c] / 2;
+                        jMatrix[r] = jMatrix[r].Select(el => el * 2).ToArray();
+                        jMatrix[r + 1] = jMatrix[r + 1].Select(el => el * 2).ToArray();
                     }
                     
                 }
