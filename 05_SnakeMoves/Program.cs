b89e156dc2a0d9005749d0103e9f411cbe0c7d8e
Vencislav <vencislav67@hotmail.com>
2022-05-26 14:24:01 +0300

    Filled the matrix

--- 05_SnakeMoves/Program.cs
+++ 05_SnakeMoves/Program.cs
@@ -7,9 +7,40 @@ namespace _05_SnakeMoves
         static void Main(string[] args)
         {
             string[] dimetions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int rows = int.Parse(dimetions[0]);
+            int cols = int.Parse(dimetions[1]);
+            string[,] matrix = new string[rows, cols];
             string snake = Console.ReadLine();
+            int index = 0;
 
-
+            for (int r = 0; r < cols; r++)
+            {
+                
+                if(r % 2 == 1)
+                {
+                    for(int c = cols-1; c >=0; c--)
+                    {
+                        matrix[r, c] = snake[index].ToString();
+                        index++;
+                        if(index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int c = 0; c < cols; c++)
+                    {
+                        matrix[r, c] = snake[index].ToString();
+                        index++;
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+            }
         }
     }
 }
