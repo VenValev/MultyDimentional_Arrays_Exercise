d12b2b1375d4d1125887af2ec6669f63f017eca1
Vencislav <vencislav67@hotmail.com>
2022-05-28 19:35:12 +0300

    Done

--- 09_Miner/Program.cs
+++ 09_Miner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _09_Miner
 {
@@ -11,14 +12,32 @@ namespace _09_Miner
             int colloectedCoal = 0;
             string[] cmnd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
             Tuple<int, int> startPosition = new Tuple<int, int>(0, 0);
-            Tuple<int, int> endPosition = new Tuple<int, int>(0, 0);
-
+            //Tuple<int, int> endPosition = new Tuple<int, int>(0, 0);
+            int countCoal = 0;
             FillingMatrix(matrix, n);
 
             startPosition = FindingStartPosition(matrix, n, startPosition);
-            endPosition = FindingEndPossition(matrix, n, endPosition);
+            //endPosition = FindingEndPossition(matrix, n, endPosition);
 
-            PlayGame(n, matrix, colloectedCoal, cmnd, startPosition);
+            countCoal = FindingCoalCount(matrix, n, countCoal);
+
+            PlayGame(n, matrix, colloectedCoal, cmnd, startPosition, countCoal);
+        }
+
+        private static int FindingCoalCount(string[,] matrix, int n, int countCoal)
+        {
+            for (int r = 0; r < n; r++)
+            {
+                
+                for (int c = 0; c < n; c++)
+                {
+                    if(matrix[r,c] == "c")
+                    {
+                        countCoal++;
+                    }
+                }
+            }
+            return countCoal;
         }
 
         private static Tuple<int, int> FindingStartPosition(string[,] matrix, int n, Tuple<int, int> startPosition)
@@ -37,7 +56,7 @@ namespace _09_Miner
             return startPosition;
         }
 
-        private static Tuple<int, int> FindingEndPossition(string[,] matrix, int n, Tuple<int, int> endPosition)
+        /*private static Tuple<int, int> FindingEndPossition(string[,] matrix, int n, Tuple<int, int> endPosition)
         {
             for (int r = 0; r < n; r++)
             {
@@ -51,31 +70,52 @@ namespace _09_Miner
                 }
             }
             return endPosition;
-        }
+        }*/
 
-        private static void PlayGame(int n, string[,] matrix, int colloectedCoal, string[] cmnd, Tuple<int, int> startPosition)
+        private static void PlayGame(int n, string[,] matrix, int colloectedCoal, string[] cmnd, Tuple<int, int> startPosition, int countCoal)
         {
-            Tuple<int, int> currentPossition = startPosition;
+            int currentRow = startPosition.Item1;
+            int currentCol = startPosition.Item2;
+
             for(int i = 0; i < cmnd.Length; i++)
             {
                 
-                if (cmnd[i] == "left" && currentPossition.Item2 - 1 >= 0)
+                if (cmnd[i] == "left" && currentCol - 1 >= 0)
                 {
-                    
+                    currentCol--;
                 }
-                else if(cmnd[i] == "right" && currentPossition.Item2 + 1 < n)
+                else if(cmnd[i] == "right" && currentCol + 1 < n)
                 {
-
+                    currentCol++;
                 }
-                else if (cmnd[i] == "up" && currentPossition.Item1 - 1 >= 0)
+                else if (cmnd[i] == "up" && currentRow - 1 >= 0)
                 {
-
+                    currentRow--;
                 }
-                else if (cmnd[i] == "down" && currentPossition.Item2 + 1 < n)
+                else if (cmnd[i] == "down" && currentRow + 1 < n)
                 {
+                    currentRow++;
+                }
 
+                if (matrix[currentRow, currentCol] == "e")
+                {
+                    Console.WriteLine($"Game over! ({currentRow}, {currentCol})");
+                    return;
+                }
+                
+                if(matrix[currentRow, currentCol] == "c")
+                {
+                    colloectedCoal++;
+                    matrix[currentRow, currentCol] = "*";
+                }
+
+                if (colloectedCoal == countCoal)
+                {
+                    Console.WriteLine($"You collected all coals! ({currentRow}, {currentCol})");
+                    return;
                 }
             }
+            Console.WriteLine($"{countCoal - colloectedCoal} coals left. ({currentRow}, {currentCol})");
         }
 
         static void FillingMatrix(string[,] matrix, int n)
