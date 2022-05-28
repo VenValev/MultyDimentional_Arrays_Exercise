a3a60f871fbceb0421ce6d2c688eb23fff5f8922
Vencislav <vencislav67@hotmail.com>
2022-05-28 12:15:09 +0300

    made some things

--- 08_Bombs/Program.cs
+++ 08_Bombs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08_Bombs
 {
@@ -11,7 +12,30 @@ namespace _08_Bombs
 
             FillingTheMatrix(matrix, n);
 
+            string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<Tuple<int, int>> bombCoordinates = new List<Tuple<int, int>>();
+            FillingCoordinates(line, bombCoordinates);
 
+            ActivatingExplosions(matrix, n, line, bombCoordinates);
+
+        }
+
+        private static void FillingCoordinates(string[] line, List<Tuple<int, int>> bombCoordinates)
+        {
+            for(int i = 0; i < line.Length; i++)
+            {
+                string[] s = line[i].Split(',', StringSplitOptions.RemoveEmptyEntries);
+                int cx = int.Parse(s[0]);
+                int cy = int.Parse(s[1]);
+                Tuple<int, int> currentC = new Tuple<int, int>(cx, cy);
+                bombCoordinates.Add(currentC);
+            }
+        }
+
+        private static void ActivatingExplosions(int[,] matrix, int n, string[] line, List<Tuple<int, int>> bombCoordinates)
+        {
+            
+            
         }
 
         private static void FillingTheMatrix(int[,] matrix, int n)
