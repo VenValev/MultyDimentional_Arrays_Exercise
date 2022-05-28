1a4fe471853a152c21ec0ba4abb17e85c409c14d
Vencislav <vencislav67@hotmail.com>
2022-05-28 18:40:55 +0300

    filling matrix

--- 09_Miner/Program.cs
+++ 09_Miner/Program.cs
@@ -8,6 +8,23 @@ namespace _09_Miner
         {
             int n = int.Parse(Console.ReadLine());
             string[,] matrix = new string[n, n];
+            int colloectedCoal = 0;
+            string[] cmnd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            FillingTheMatrix(matrix, n);
+
+        }
+
+        private static void FillingTheMatrix(string[,] matrix, int n)
+        {
+            
+            for(int r = 0; r < n; r++)
+            {
+                string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for(int c = 0; c < n; c++)
+                {
+
+                }
+            }
         }
     }
 }
